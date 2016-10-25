b2dfb93ff4e9c36af89c3e91e2fde1c78e89846f
raquel <raquelpa93@gmail.com>
2016-10-25 19:13:32 +0200

    Added a test of baseCad and some polys

--- scriptVagos.py
+++ scriptVagos.py
@@ -29,3 +29,30 @@ print("")
 
 for c in stack2.cells:
     print(c.sample)
+
+
+#test para varias variables
+
+
+x1 = symbols('x1')
+x2 = symbols('x2')
+
+# Example 1
+
+poly1 = Poly(x1 + 1)
+poly2 = Poly(x1 - 1)
+poly3 = Poly(2*x1**2 - 1)
+polys = [poly1, poly2, poly3]
+
+stack3 = Stack(None, polys)
+
+print("EJEMPLO3")
+
+#for c in stack3.cells:
+    # print(c.sample)
+
+
+cadMuestra = baseCad(polys)
+print("c sample:")
+for c in cadMuestra.cells:
+    print(c.sample)
