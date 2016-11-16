0b3b764efb1c2ad43004fbc64787ecf1e4155968
Loirooriol <obrufau@hotmail.com>
2016-11-16 13:32:31 +0100

    Return set instead of list

--- subresultants.py
+++ subresultants.py
@@ -7,7 +7,7 @@ import sympy
 from sympy import subresultants, LC, degree
 
 def PSC(F, G, x):
-    """PSC(F, G, x) returns the list of the non-zero principal subresultant
+    """PSC(F, G, x) returns a set with the non-zero principal subresultant
     coefficients (psc) of the two polynomials F and G with respect to the
     variable x.
     
@@ -16,22 +16,22 @@ def PSC(F, G, x):
     considered.
 
     >>> PSC(0, 0, var('y'))
-    []
+    set()
     >>> PSC(poly('2*y'), poly('3*x'), var('y'))
-    [1]
+    {1}
     >>> PSC(poly('2*y'), poly('3*x + 5*y**2'), var('y'))
-    [12*x, 1]
+    {12*x, 1}
     >>> PSC(poly('y**3'), poly('y**3 + y'), var('y'))
-    [1, 1]
+    {1}
     """
+    s = set()
     n = min( degree(F,x), degree(G,x) )
     if n < 0:
-        return []
+        return s
     subs = subresultants(F, G, x)[2:]
-    s = []
     for p in reversed(subs):
-        s.append( LC(p, x) )
-    s.append(1); # SIAM defines it as 1 but WolframAlpha sometimes says otherwise
+        s.add( LC(p, x) )
+    s.add(1); # SIAM defines it as 1 but WolframAlpha sometimes says otherwise
     return s
 
 
