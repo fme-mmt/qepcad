231338e80130829cc9a30b80e32af917d0858524
Loirooriol <obrufau@hotmail.com>
2016-12-07 14:27:47 +0100

    Now it makes sense!

--- subresultants.py
+++ subresultants.py
@@ -11,12 +11,11 @@ def PSC(F, G, x):
     coefficients (psc) of the two polynomials F and G with respect to the
     variable x.
     
-    The n-th psc, where n is the minimum of the degrees of F and G with
-    respect to x, is defined as 1 when F and G have the same degree.
-    Extended psc beyond the n-th are not considered.
-    
     If the degree of the polynomial F is strictly less than the degree of
     the polynomial G, then F and G are interchanged.
+    
+    Extended psc beyond the n-th are not considered, where n is the minimum
+    of the degrees of F and G with respect to x.
 
     >>> PSC(0, 0, var('x'))
     set()
@@ -27,21 +26,17 @@ def PSC(F, G, x):
     >>> PSC(poly('x**3'), poly('x**3 + x'), var('x'))
     {1}
     """
-    degF = degree(F, x)
-    degG = degree(G, x)
-    if degF < degG:
-        [F, G] = [G, F]
-        [degF, degG] = [degG, degF]
     subs = subresultants(F, G, x)
     s = set()
     i = len(subs) - 1
-    while i > 1:
-        coef = LC(subs[i], x)
-        degS = degree(subs[i], x)
-        s.add( coef**(degG-i+2-degS) )
+    if i < 0:
+        return s
+    currDeg = degree(subs[i], x)
+    while i > 0:
+        nextDeg = degree(subs[i-1], x)
+        s.add( LC(subs[i], x)**(nextDeg-currDeg) )
+        currDeg = nextDeg
         i -= 1
-    if i > 0:
-        s.add( LC(subs[i], x)**(degF-degG) )
     return s
 
 
